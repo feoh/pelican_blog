78b4b2de23f13959a7396405317f5512097f5de2
Chris Patti <cpatti@gmail.com>
2015-06-20 00:40:25 -0400

    Add some basic Chefspec tests. No ServerSpec yet.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,16 +8,23 @@ require 'spec_helper'
 
 describe 'pelican_blog::default' do
 
-  context 'When all attributes are default, on an unspecified platform' do
+  before do
+    stub_command('which nginx').and_return(nil)
+  end
 
+  context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       chef_run # This should not raise an error
     end
-
+    it 'installs nginx' do
+      expect(chef_run).to install_package('nginx')
+    end
+    it 'installs git' do
+      expect(chef_run).to install_package('git')
+    end
   end
 end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,9 +4,15 @@ describe 'pelican_blog::default' do
 
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-
+  # it 'provides a working nginx server listening on port 80' do
+  #   describe port(80) do
+  #     it { should be_listening }
+  #   end
+  # end
+  # it 'installs configured and runs nginx' do
+  #   describe service('nginx'), :if => os[:family] == 'redhat' do
+  #     it { should be_enabled }
+  #     it { should be_running }
+  #   end
+  # end
 end
