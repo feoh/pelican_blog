35b3d3c7b2690845ca39cfd852b0e66f7c635e02
Chris Patti <cpatti@gmail.com>
2015-06-19 23:26:19 -0400

    Actually run the site generator with each chef client run.

--- Berksfile
+++ Berksfile
@@ -1,5 +1,6 @@
 source "https://supermarket.chef.io"
 
+cookbook "python"
 cookbook "nginx"
 cookbook "chef-client"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,12 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+include_recipe "python"
 include_recipe "nginx"
 
 package "git"
+python_pip "markdown"
+python_pip "pelican"
 
 bash "clean_house" do
   code <<-EOH
@@ -36,6 +39,15 @@ git "/tmp/blindnotdumb" do
   enable_submodules "true"
   revision "master"
   action :sync
+  notifies :run, "execute[publish_site]"
+end
+
+
+
+
+execute "publish_site" do
+  cwd "/tmp/blindnotdumb"
+  command "make publish"
   notifies :run, "execute[copy_site_dir]"
 end
 
